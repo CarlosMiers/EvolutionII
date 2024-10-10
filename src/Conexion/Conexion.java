2afab1e8ec64fe6f9d9413a58ce1124b2eedccd5
unknown <carlosgonzalezmiers@gmail.com>
2024-10-10 09:41:38 -0300

    FAIS FACTURACION

--- src/Conexion/Conexion.java
+++ src/Conexion/Conexion.java
@@ -33,13 +33,13 @@ public class Conexion {
 //conn = DriverManager.getConnection("jdbc:mysql://45.180.183.152/evolution2021", "root", "1541947"); //SUCURSAL 2 FERREMAX   
 //conn = DriverManager.getConnection("jdbc:mysql://" + Config.cIpServer.trim() + "/Evolution", "root", "1541947");
 //  conn = DriverManager.getConnection("jdbc:mysql://" + Config.cIpServer.trim()+":3307" + "/Evolution2021", "root", "1541947");
-//conn = DriverManager.getConnection("jdbc:mysql://" + Config.cIpServer.trim()+ "/Evolution2021", "root", "1541947");
+conn = DriverManager.getConnection("jdbc:mysql://" + Config.cIpServer.trim()+ "/Evolution2021", "root", "1541947");
 //conn = DriverManager.getConnection( "jdbc:mysql://Localhost/rmachado", "root", "1541947");
 //      conn = DriverManager.getConnection("jdbc:mysql://Localhost/colegio","root", "1541947");
 //    conn = DriverManager.getConnection( "jdbc:mysql://Localhost/evolution2021", "root", "1541947");
 //             conn = DriverManager.getConnection( "jdbc:mysql://Localhost/evolution2021", "root", "1541947");
 
-               conn = DriverManager.getConnection( "jdbc:mysql://Localhost/financiera", "root", "1541947");
+//               conn = DriverManager.getConnection( "jdbc:mysql://Localhost/financiera", "root", "1541947");
  //            conn = DriverManager.getConnection( "jdbc:mysql://Localhost/rmachado", "root", "1541947");
 //     conn = DriverManager.getConnection("jdbc:mysql://181.78.27.142:3307/evolution2021", "root", "1541947"); 
 
